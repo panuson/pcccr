ce1a9e5acc927dffe95e349fc53adbd28d40614d
panuson <36943093+panuson@users.noreply.github.com>
2020-01-30 12:55:43 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,289 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/style.min.css">
+    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+    <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+    <link href="assets/css/all.min.css" rel="stylesheet"> 
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+<!-----------------------------------------------------------start header-------------------------------------------------------------------->
+    <nav class="navbar navbar-expand-lg bg-orange">
+        <div class="container-lg">
+            <small class="text-white text-center text-lg-right text-md-center text-sm-center"style="width: 100%;">345 หมู่ 2 ต.รอบเวียง อ.เมือง จ.เชียงราย 57000  |   โทร 053-174551-4   |    โทรสาร 053-174-555</small>
+        </div>
+    </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-white flex-column flex-md-row">
+        <div class="container-lg">
+            <a class="text-lg-left text-md-center mr-1 mx-auto my-sm-2" href="#">
+            <img src="assets/img/logo.svg" width="85" height="85" alt="" >
+            </a>
+            <div class="text-center text-lg-left text-md-center text-sm-center w-100">
+            <span>โรงเรียนวิทยาศาสตร์จุฬาภรณราชวิทยาลัย เชียงราย</span><br>
+            <small>Princess Chulabhorn Science High School Chiang Rai</small>
+        </div>
+        
+        <div class="w-100">
+            <button class="navbar-toggler w-100 mt-sm-2" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+                <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto text-center">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle navlinkcus" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        ข้อมูลทั่วไป
+                        </a>
+                        <div class="dropdown-menu dropcus" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">หน้าหลัก</a>
+                            <a class="dropdown-item" href="#">ประวัติโรงเรียน</a>
+                            <a class="dropdown-item" href="#">เอกลักณ์ อัตลักษณ์</a>
+                            <a class="dropdown-item" href="#">นโยบายการบริหาร</a>
+                            <a class="dropdown-item" href="#">คณะกรรมการสถานศึกษา</a>
+                            <a class="dropdown-item" href="#">บุคลากรกลุ่มบริหาร</a>
+                            <a class="dropdown-item" href="#">บุคลากรกลุ่มครูผู้สอน</a>
+                            <a class="dropdown-item" href="#">บุคลากรกลุ่มปฏิบัติการ</a>
+                            <a class="dropdown-item" href="#">แผนที่โรงเรียน</a>
+
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle navlinkcus" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        ระบบงาน
+                        </a>
+                        <div class="dropdown-menu dropcus" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">E-School</a>
+                            <a class="dropdown-item" href="#">E-Money</a>
+                            <a class="dropdown-item" href="#">Library</a>
+                            <a class="dropdown-item" href="#">CSG</a>
+                            <a class="dropdown-item" href="#">ระบบกรอกคะแนน SGS</a>
+                            <a class="dropdown-item" href="#">Microsoft Office 365</a>
+                            
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle navlinkcus" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        กลุ่มงานฯ
+                        </a>
+                        <div class="dropdown-menu dropcus" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle navlinkcus" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        ห้องเกียรติยศ
+                        </a>
+                        <div class="dropdown-menu dropcus" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle navlinkcus" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        กิจกรรม
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+    
+    <div class="single-item">
+        <img src="assets/img/banner/1.jpg" alt="">
+        <img src="assets/img/banner/2.jpg" alt="">
+    </div>    
+<!-----------------------------------------------------------end header-------------------------------------------------------------------->
+
+<!---------------------------------------------------------------Menu-------------------------------------------------------------------->
+
+<div class="oftenmenu">
+    <div class="container-lg">
+        <div class="row py-4 text-center text-white">
+            <div class="col-6 col-sm-4 col-md-3 col-lg my-2 my-sm-3 my-md-4 my-lg-0  ">
+                <a class="nav-link text-white link py-3" href="http://">
+                    <i class="fas fa-money-check-alt h2"></i>
+                    <div><small>E-Money</small></div>
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg my-2 my-sm-3 my-md-4 my-lg-0  ">
+                <a class="nav-link text-white link py-3" href="http://">
+                    <i class="fas fa-school h2"></i>
+                    <div><small>E-School</small></div>
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg my-2 my-sm-3 my-md-4 my-lg-0  ">
+                <a class="nav-link text-white link py-3" href="http://">
+                    <i class="fas fa-list-ol h2"></i>
+                    <div><small>ระบบคะแนน SGS</small></div>
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg my-2 my-sm-3 my-md-4 my-lg-0  ">
+                <a class="nav-link text-white link py-3" href="http://">
+                    <i class="fas fa-info-circle h2"></i>
+                    <div><small>ระบบ PCSHS</small></div>
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg my-2 my-sm-3 my-md-4 my-lg-0  ">
+                <a class="nav-link text-white link py-3" href="http://">
+                    <i class="fas fa-calendar-alt h2"></i>
+                    <div><small>ตารางกลับบ้าน</small></div>
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg my-2 my-sm-3 my-md-4 my-lg-0  ">
+                <a class="nav-link text-white link py-3" href="http://">
+                    <i class="fas fa-book h2"></i>
+                    <div><small>ตารางเรียน</small></div>
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg my-2 my-sm-3 my-md-4 my-lg-0  ">
+                <a class="nav-link text-white link py-3" href="http://">
+                    <i class="fas fa-utensils h2"></i>
+                    <div><small>รายการอาหาร</small></div>
+                </a>
+            </div>
+            <div class="col-6 col-sm-4 col-md-3 col-lg my-2 my-sm-3 my-md-4 my-lg-0  ">
+                <a class="nav-link text-white link py-3" href="http://">
+                    <i class="fas fa-user-friends h2"></i>
+                    <div><small>รายชื่อนักเรียน</small></div>
+                </a>
+            </div>
+           
+        </div>
+    </div>
+</div>
+<!---------------------------------------------------------------end Menu-------------------------------------------------------------------->
+
+<!---------------------------------------------------------------content-------------------------------------------------------------------->
+
+<div class="container-xl my-3">
+    <div class="banner">
+    <h2 class="display-4 text-center">Banner</h2>
+    </div>
+</div>
+
+    <div class="section my-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12 col-md-8 mb-sm-3">
+            <div class="title-lt-wd border-bottom">
+                <h5 class="underlinetext">ข่าวประชาสัมพันธ์</h5>
+            </div>
+            <div class="card-deck my-4">
+                <div class="card sm-12">
+                    <img src="assets/img/test.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">ทดสอบ</h5>
+                    <small class="card-text">การแข่งขันตอบคำถามสารานุกรมไทยสำหรับเยาวชนฯ ครั้งที่ 25 ระดับจังหวัด วันที่ 24 </small>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+                <div class="card sm-12">
+                    <img src="assets/img/test.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">ทดสอบ</h5>
+                    <small class="card-text">ขอแสดงความยินดีกับนักเรียนได้เข้ารอบไปแข่งขันต่อในระดับภาค ได้แก่ เด็กหญิงปภา....</small>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+                <div class="card sm-12">
+                    <img src="assets/img/test.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">ทดสอบ</h5>
+                    <small class="card-text">ขอแสดงความยินดีกับนักเรียนได้เข้ารอบไปแข่งขันต่อในระดับภาค ได้แก่ เด็กหญิงปภา.</small>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div> 
+
+           <hr>
+           <div class="text-right more">
+                <a class="" href="http://"><small>ดูกิจกรรมเพิ่มเติม</small></a>
+            </div>
+            <div class="title-lt-wd border-bottom">
+                <h5 class="underlinetext">กิจกรรม</h5>
+            </div>
+            <div class="card-deck my-4">
+                <div class="card sm-12">
+                    <img src="assets/img/test.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">ทดสอบ</h5>
+                    <small class="card-text">การแข่งขันตอบคำถามสารานุกรมไทยสำหรับเยาวชนฯ ครั้งที่ 25 ระดับจังหวัด วันที่ 24 </small>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+                <div class="card sm-12">
+                    <img src="assets/img/test.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">ทดสอบ</h5>
+                    <small class="card-text">ขอแสดงความยินดีกับนักเรียนได้เข้ารอบไปแข่งขันต่อในระดับภาค ได้แก่ เด็กหญิงปภา....</small>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+                <div class="card sm-12">
+                    <img src="assets/img/test.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title">ทดสอบ</h5>
+                    <small class="card-text">ขอแสดงความยินดีกับนักเรียนได้เข้ารอบไปแข่งขันต่อในระดับภาค ได้แก่ เด็กหญิงปภา.</small>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div> 
+           <hr>
+           <div class="text-right more">
+                <a class="" href="http://"><small>ดูกิจกรรมเพิ่มเติม</small></a>
+            </div>
+            
+            </div>
+            <div class="col-sm-12 col-md-4 my-0">
+                <div class="title-lt-wd border-bottom text-center ">
+                    <h5 class="nounderln">ผู้อำนวยการโรงเรียน</h5>
+                </div>
+                <div class="text-center my-4 dir">
+                    <img class="" src="assets/img/pradit.png" alt="" width="240px"">
+                    <div class="tabcenter py-1 mt-1 text-center">
+                        <p class="my-auto text-white">นายประดิษฐ์ โพธิกลาง</p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+
+
+
+    </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="slick/slick.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('.single-item').slick({
+            dots: false,
+            touchMove: true,
+            infinite: true,
+            autoplay: true,
+            autoplaySpeed: 5000,
+            fade: true,
+            cssEase: 'linear',
+            arrows: false,
+            });
+        });
+    </script>
+  </body>
+</html>
